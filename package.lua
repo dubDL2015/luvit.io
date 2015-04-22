27afd78d644516dd4a3fae0efc2a022741bf1192
Tim Caswell <tim@creationix.com>
2015-04-22 16:36:41 -0500

    Move hybrid-fs to lit

--- package.lua
+++ package.lua
@@ -1,11 +1,12 @@
 return {
-  name = "luvit/blog.luvit.io",
+  name = "luvit/luvit.io",
   version = "0.0.1",
   private = true,
   dependencies = {
     "luvit/require",
     "luvit/pretty-print",
     "luvit/json",
+    "creationix/hybrid-fs@0.1.0",
     -- hoedown ffi bindings for fast markdown compiling
     "creationix/hoedown@1.0.1",
     -- Web server framework
@@ -13,7 +14,7 @@ return {
     -- Core plugin for proper http headers
     "creationix/weblit-auto-headers@0.1.0",
     -- Serve static files from disk
-    "creationix/weblit-static@0.2.0",
+    "creationix/weblit-static@0.3.0",
     -- In-memory caching of http responses
     "creationix/weblit-etag-cache@0.1.0",
     -- Basic logger to stdout
